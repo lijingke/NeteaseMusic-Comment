6af0a9f251ec6c9e8e60718aa86145ed6b8ac248
李京珂 <lijingke@hotmail.com>
2020-07-02 16:40:27 +0800

    init

---
+++ NeteaseMusic Comment/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NeteaseMusic Comment
+//
+//  Created by 李京珂 on 2020/7/2.
+//  Copyright © 2020 李京珂. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ NeteaseMusic Comment/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  NeteaseMusic Comment
+//
+//  Created by 李京珂 on 2020/7/2.
+//  Copyright © 2020 李京珂. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    lazy var mainView: HomeView = {
+        let view = HomeView()
+        return view
+    }()
+    
+}
+
+extension HomeViewController {
+    private func setupUI() {
+        view.addSubview(mainView)
+        
+    }
+}
